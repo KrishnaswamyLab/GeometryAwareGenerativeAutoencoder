f31cfdde919e9a43e82ec104acb3058e3ba6a796
Xingzhi Sun <xingzhi.sun@yale.edu>
2024-05-02 21:16:40 -0400

    data genenation

--- src/data_convert.py
+++ src/data_convert.py
@@ -26,8 +26,32 @@ import hydra
 
 # to simulate the application case, do not include test points when running phate for training points.
 # but include training points when running phate for test points.
-def convert_data(X, seed=42, test_size=0.2):
-    phate_op = phate.PHATE()
+# def convert_data(X, seed=42, test_size=0.2):
+#     phate_op = phate.PHATE()
+#     phate_data = phate_op.fit_transform(X)
+#     colors = np.zeros(X.shape[0])
+#     dists_all = squareform(pdist(phate_op.diff_potential))
+#     ids = np.arange(0, X.shape[0])
+#     id_train, id_test = train_test_split(ids, test_size=test_size, random_state=seed)
+#     is_train = np.isin(ids, id_train)
+#     X_train = X[is_train]
+#     phate_op = phate.PHATE()
+#     _ = phate_op.fit_transform(X_train)
+#     dists = dists_all.copy()
+#     dists_train = squareform(pdist(phate_op.diff_potential))
+#     dists[is_train][:,is_train] = dists_train
+#     return dict(
+#         data=X,
+#         colors=colors,
+#         dist=dists,
+#         dist_all=dists_all, # deprecated.
+#         phate=phate_data,
+#         is_train=is_train
+#     )
+
+def convert_data(X, seed=42, test_size=0.2, knn=5, t=30, n_components=3, decay=40):
+    # phate_op = phate.PHATE(random_state=42, knn=20, t=30, n_components=3, decay=5)
+    phate_op = phate.PHATE(random_state=seed, t=t, n_components=n_components, knn=knn, decay=decay)
     phate_data = phate_op.fit_transform(X)
     colors = np.zeros(X.shape[0])
     dists_all = squareform(pdist(phate_op.diff_potential))
@@ -35,7 +59,8 @@ def convert_data(X, seed=42, test_size=0.2):
     id_train, id_test = train_test_split(ids, test_size=test_size, random_state=seed)
     is_train = np.isin(ids, id_train)
     X_train = X[is_train]
-    phate_op = phate.PHATE()
+    # phate_op = phate.PHATE(random_state=seed, knn=20, t=30, n_components=3, decay=5)
+    phate_op = phate.PHATE(random_state=seed, t=t, n_components=n_components, knn=knn, decay=decay)
     _ = phate_op.fit_transform(X_train)
     dists = dists_all.copy()
     dists_train = squareform(pdist(phate_op.diff_potential))
